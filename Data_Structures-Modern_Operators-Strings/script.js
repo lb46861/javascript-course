b5006d3ff6f36ffc90239be76124a884d30964cb
unknown <begovic.lovre@gmail.com>
2021-11-06 16:29:18 +0100

    spread operator

--- Data_Structures-Modern_Operators-Strings/script.js
+++ Data_Structures-Modern_Operators-Strings/script.js
@@ -41,7 +41,67 @@ const restaurant = {
       `Order recieved! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} by ${time}.`
     );
   },
+
+  orderPasta: function (ing1, ing2, ing3) {
+    console.log(
+      `Here is your delicious pasta with ${ing1}, ${ing2} and ${ing3}.`
+    );
+  },
 };
+
+const arr = [7, 8, 9];
+const badNewArr = [1, 2, arr[0], arr[1], arr[2]];
+console.log(badNewArr);
+
+// Spread operator
+const goodNewArr = [1, 2, ...arr];
+console.log(goodNewArr);
+
+console.log(...goodNewArr);
+console.log(1, 2, 7, 8, 9);
+
+const newMenu = [...restaurant.mainMenu, 'Chicken', 'Gnocci'];
+console.log(newMenu);
+
+// Copy array
+const mainMenuCopy = [...restaurant.mainMenu];
+
+// Join 2 arrays
+const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+console.log(menu);
+
+// Spread operator works on all types of iterables, most of built in data structures except objects
+
+// Iterables : arrays, strings, maps, sets
+const str = 'Jonas';
+const letters = [...str, ' ', 'S.'];
+console.log(letters);
+// We can use spread operator ONLY when building an array or when we pass values into a function
+console.log(...str);
+// console.log(`${...str} Schmedtman`); // This will not work
+
+// Example how to use spread operator
+const ingredients = [
+  // prompt("Let's make pasta! Ingredient 1 ?"),
+  // prompt(' Ingredient 2 ?'),
+  // prompt('Ingredient 3 ?'),
+];
+console.log(ingredients);
+
+restaurant.orderPasta(...ingredients);
+
+// Objects
+const newRestaurant = {
+  foundingYear: 1980,
+  ...restaurant,
+  founder: 'Guisseppe',
+};
+console.log(newRestaurant);
+const restaurantCopy = { ...restaurant };
+restaurantCopy.name = 'Palace';
+console.log(restaurantCopy.name + ' - ' + restaurant.name);
+/*
+// Object destructing
 
 restaurant.orderDelivery({
   time: '22:30',
@@ -55,7 +115,7 @@ restaurant.orderDelivery({
   starterIndex: 1,
 });
 
-// Object destructing
+
 const { name, openingHours, categories } = restaurant;
 console.log(name, openingHours, categories);
 
@@ -84,7 +144,7 @@ const {
 } = openingHours;
 console.log(openHours, closeHours);
 
-/*
+
 
 // Destructing arrays
 const arr = [2, 3, 4];
