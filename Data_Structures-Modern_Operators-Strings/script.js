eea8ea6bab955e5997c525385b9d60df2bee030f
unknown <begovic.lovre@gmail.com>
2021-11-09 17:22:43 +0100

    Coding challenge

--- Data_Structures-Modern_Operators-Strings/script.js
+++ Data_Structures-Modern_Operators-Strings/script.js
@@ -54,6 +54,83 @@ const restaurant = {
   },
 };
 
+const game = {
+  team1: 'Bayern Munich',
+  team2: 'Borrussia Dortmund',
+  players: [
+    [
+      'Neuer',
+      'Pavard',
+      'Martinez',
+      'Alaba',
+      'Davies',
+      'Kimmich',
+      'Goretzka',
+      'Coman',
+      'Muller',
+      'Gnarby',
+      'Lewandowski',
+    ],
+    [
+      'Burki',
+      'Schulz',
+      'Hummels',
+      'Akanji',
+      'Hakimi',
+      'Weigl',
+      'Witsel',
+      'Hazard',
+      'Brandt',
+      'Sancho',
+      'Gotze',
+    ],
+  ],
+  score: '4:0',
+  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+  date: 'Nov 9th, 2037',
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+
+  printGoals: function (...players) {
+    let sum = 0;
+    for (let i = 0; i < players.length; i++) {
+      console.log(players[i]);
+      sum += 1;
+    }
+    console.log(`Total goals: ${sum}`);
+  },
+};
+
+const [players1, players2] = game.players;
+//console.log(players1, players2);
+
+const [gk, ...fieldPlayers] = players1;
+//console.log(gk, fieldPlayers);
+
+const allPlayers = [...players1, ...players2];
+//console.log(allPlayers);
+
+const playersFinal = [...players1, 'Thiago', 'Coutinho', 'Perisic'];
+//console.log(playersFinal);
+
+// const {
+//   odds: { team1, x: draw, team2 },
+// } = game;
+// console.log(team1, draw, team2);
+
+const { team1, x: draw, team2 } = game.odds;
+console.log(team1, draw, team2);
+
+// game.printGoals('Davies', 'Muller', 'Lewandowski', 'Kimmich');
+//game.printGoals(...game.scored);
+
+team1 < team2 && console.log('Team 1 is more likely to win');
+team1 > team2 && console.log('Team 2 is more likely to win');
+
+/*
 restaurant.numGuests = 0;
 const guests = restaurant.numGuests || 10;
 console.log(guests);
@@ -62,7 +139,7 @@ console.log(guests);
 const guestsCorrect = restaurant.numGuests ?? 10;
 console.log(guestsCorrect);
 
-/*
+
 
 console.log('--- OR ---');
 // LOGICAL OPERATORS
