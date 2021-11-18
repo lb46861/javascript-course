d9013e7bf3dfd66cc637e0cf72e7e4a9c8941aa9
unknown <begovic.lovre@gmail.com>
2021-11-18 23:42:43 +0100

    Practice for strings

--- Data_Structures-Modern_Operators-Strings/script.js
+++ Data_Structures-Modern_Operators-Strings/script.js
@@ -4,6 +4,25 @@
 const flights =
   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
 
+// 🔴 Delayed Departure from FAO to TXL (11h25)
+//              Arrival from BRU to FAO (11h45)
+//   🔴 Delayed Arrival from HEL to FAO (12h05)
+//            Departure from FAO to LIS (12h30)
+
+const flightsArray = flights.split('+');
+
+const getCode = str => str.slice(0, 3).toUpperCase();
+for (const flight of flightsArray) {
+  const [type, from, to, time] = flight.split(';');
+  const output = `${type.startsWith('_Delayed') ? '🔴 ' : ''}${type
+    .replaceAll('_', ' ')
+    .trim()} from ${getCode(from)} to ${getCode(to)} (${time.replace(
+    ':',
+    'h'
+  )})`.padStart(44, ' ');
+  console.log(output);
+}
+
 // Data needed for first part of the section
 
 const weekDays = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
@@ -55,6 +74,8 @@ const restaurant = {
   },
 };
 
+/* 
+
 document.body.append(document.createElement('textarea'));
 document.body.append(document.createElement('button'));
 
@@ -70,7 +91,7 @@ document.querySelector('button').addEventListener('click', function () {
   }
 });
 
-/* 
+
 console.log('a+very+nice+string'.split('+'));
 console.log('Lovre Begs'.split(' '));
 
