f1b0ed7ee0208bca5e1ba3a4be2d5ab1d9b03c24
unknown <begovic.lovre@gmail.com>
2021-11-23 14:16:36 +0100

    Passing arguments to functions

--- Functions/script.js
+++ Functions/script.js
@@ -1,5 +1,6 @@
 'use strict';
 
+/*
 const bookings = [];
 const createBooking = function (
   flightNum,
@@ -23,3 +24,32 @@ createBooking('LH123', 2, 800);
 createBooking('LH123', 2);
 createBooking('LH123', 5);
 createBooking('LH123', undefined, 1000);
+*/
+
+const flight = 'LH234';
+const lovre = {
+  name: 'Lovre B',
+  passport: 8591431243,
+};
+
+const checkIn = function (flightNum, passenger) {
+  flightNum = 'LH999';
+  passenger.name = 'Mr. ' + passenger.name;
+
+  if (passenger.passport === 8591431243) alert('Checked in');
+  else alert('Wrong passport');
+};
+
+checkIn(flight, lovre);
+// const flightNum = flight;
+// const passenger = jonas;
+console.log(flight);
+console.log(lovre);
+
+const newPassport = function (person) {
+  person.passport = Math.trunc(Math.random() * 1000000);
+};
+
+newPassport(lovre);
+checkIn(flight, lovre);
+// We are passing the reference TO the function but not BY the function !
