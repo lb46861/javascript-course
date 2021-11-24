9b2501b89ff6b60c41af11dddd27add11fe2b048
unknown <begovic.lovre@gmail.com>
2021-11-24 17:29:27 +0100

    Functions accepting callback functions

--- Functions/script.js
+++ Functions/script.js
@@ -24,7 +24,7 @@ createBooking('LH123', 2, 800);
 createBooking('LH123', 2);
 createBooking('LH123', 5);
 createBooking('LH123', undefined, 1000);
-*/
+
 
 const flight = 'LH234';
 const lovre = {
@@ -53,3 +53,37 @@ const newPassport = function (person) {
 newPassport(lovre);
 checkIn(flight, lovre);
 // We are passing the reference TO the function but not BY the function !
+
+*/
+
+const oneWord = function (str) {
+  //return str.replaceAll(' ', '').toLowerCase();
+  return str.replaceAll(/ /g, '').toLowerCase();
+};
+
+const upperFirstWord = function (str) {
+  const [first, ...other] = str.split(' ');
+  return [first.toUpperCase(), ...other].join(' ');
+};
+
+// Higher-order function
+const transformer = function (str, fn) {
+  console.log(`Original string: ${str}`);
+  console.log(`Transformed string: ${fn(str)}`);
+
+  console.log(`Transformed by function: ${fn.name}`);
+};
+
+transformer('JavaScript is the best!', upperFirstWord);
+transformer('JavaScript is the best!', oneWord);
+// in transformer function we are passing the call back function and then transformer function will call those call back function
+
+// JS uses callbacks all the time
+const high5 = function () {
+  console.log('👋');
+};
+
+document.body.addEventListener('click', high5);
+// in this example addEventListener is High-Order function and high5 is call-back function
+
+['lovre', 'stipe', 'duje'].forEach(high5);
