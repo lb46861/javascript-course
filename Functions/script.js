61199105a619387bdeaecff758d93e3309c89759
unknown <begovic.lovre@gmail.com>
2021-11-30 16:35:55 +0100

    simple coding challenge - closures

--- Functions/script.js
+++ Functions/script.js
@@ -291,7 +291,7 @@ booker();
 
 console.dir(booker);
 
-*/
+
 
 // Example 1
 let f;
@@ -334,3 +334,19 @@ const boardPassengers = function (n, wait) {
 // CLOSURE even has priority over the SCOPE chain
 const perGroup = 1000;
 boardPassengers(180, 3);
+
+*/
+
+// Your tasks:
+// 1. Take the IIFE below and at the end of the function, attach an event listener that
+// changes the color of the selected h1 element ('header') to blue, each time
+// the body element is clicked. Do not select the h1 element again!
+
+// Coding challenge #2
+(function () {
+  const header = document.querySelector('h1');
+  header.style.color = 'red';
+  document.querySelector('body').addEventListener('click', function () {
+    header.style.color = 'blue';
+  });
+})();
