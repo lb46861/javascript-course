4b5f4951256f57c32a502db7f242875a7eb32b9d
unknown <begovic.lovre@gmail.com>
2021-12-01 23:13:42 +0100

    forEach in MAPS and SETS

--- Working with Arrays/script.js
+++ Working with Arrays/script.js
@@ -65,12 +65,6 @@ const inputClosePin = document.querySelector('.form__input--pin');
 /////////////////////////////////////////////////
 // LECTURES
 
-const currencies = new Map([
-  ['USD', 'United States dollar'],
-  ['EUR', 'Euro'],
-  ['GBP', 'Pound sterling'],
-]);
-
 // const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
@@ -114,29 +108,48 @@ console.log([...arr, ...arr2]);
 // Does NOT MUTATE the original array
 console.log(letters.join('-'));
 
-*/
 
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 // for (const movement of movements) {
-for (const [i, movement] of movements.entries()) {
-  if (movement > 0) {
-    console.log(`Movement ${i + 1}: You deposited ${movement}`);
-  } else {
-    console.log(`Movement ${i + 1}: You withrew ${Math.abs(movement)}`);
+  for (const [i, movement] of movements.entries()) {
+    if (movement > 0) {
+      console.log(`Movement ${i + 1}: You deposited ${movement}`);
+    } else {
+      console.log(`Movement ${i + 1}: You withrew ${Math.abs(movement)}`);
+    }
   }
-}
-console.log('--- FOREACH ---');
-movements.forEach(function (movement, i, arr) {
-  if (movement > 0) {
-    console.log(`Movement ${i + 1}: You deposited ${movement}`);
-  } else {
-    console.log(`Movement ${i + 1}: You withrew ${Math.abs(movement)}`);
-  }
-});
-// 0: function(200)
-// 1: function(450)
-// 2: function(400)
-// ...
+  console.log('--- FOREACH ---');
+  movements.forEach(function (movement, i, arr) {
+    if (movement > 0) {
+      console.log(`Movement ${i + 1}: You deposited ${movement}`);
+    } else {
+      console.log(`Movement ${i + 1}: You withrew ${Math.abs(movement)}`);
+    }
+  });
+  // 0: function(200)
+  // 1: function(450)
+  // 2: function(400)
+  // ...
+  
+  // YOU CANOT BREAK OUT OF FOREACH LOOP, ONLY IN FOROF LOOP
 
-// YOU CANOT BREAK OUT OF FOREACH LOOP, ONLY IN FOROF LOOP
+*/
+
+// MAP
+const currencies = new Map([
+  ['USD', 'United States dollar'],
+  ['EUR', 'Euro'],
+  ['GBP', 'Pound sterling'],
+]);
+
+currencies.forEach(function (value, key, map) {
+  // similiar as in array ( element, index, whole array)
+  console.log(`${key}: ${value}`);
+});
+
+// SET
+const currenciesUnique = new Set(['USD', 'GBP', 'USD', 'EUR', 'EUR', 'EUR']);
+currenciesUnique.forEach(function (value, _, map) {
+  console.log(`${value}: ${value}`);
+});
