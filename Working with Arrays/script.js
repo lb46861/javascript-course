1373cb37fa34215b89e7852d1c18803a8e324be4
unknown <begovic.lovre@gmail.com>
2021-12-02 01:26:12 +0100

    Project Bankist, Creating DOM elements

--- Working with Arrays/script.js
+++ Working with Arrays/script.js
@@ -61,6 +61,24 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
+const displayMovements = function (movements) {
+  containerMovements.innerHTML = '';
+
+  movements.forEach(function (mov, i) {
+    const type = mov > 0 ? 'deposit' : 'withdrawal';
+    const html = ` 
+    <div class="movements__row">
+    <div class="movements__type movements__type--${type}">${i + 1} ${type}</div>
+    <div class="movements__value">${mov}</div>
+    </div>
+  `;
+    containerMovements.insertAdjacentHTML('afterbegin', html);
+  });
+};
+displayMovements(account1.movements);
+
+// Using innerHTML means that any JavaScript references to the descendants of element will be removed. When you use insertAdjacentHTML , adding additional content will not corrupt the existing JS references and the existing nodes are not altered.
+
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
@@ -134,22 +152,23 @@ const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
   
   // YOU CANOT BREAK OUT OF FOREACH LOOP, ONLY IN FOROF LOOP
 
-*/
-
-// MAP
-const currencies = new Map([
-  ['USD', 'United States dollar'],
-  ['EUR', 'Euro'],
-  ['GBP', 'Pound sterling'],
-]);
-
-currencies.forEach(function (value, key, map) {
-  // similiar as in array ( element, index, whole array)
-  console.log(`${key}: ${value}`);
-});
-
-// SET
-const currenciesUnique = new Set(['USD', 'GBP', 'USD', 'EUR', 'EUR', 'EUR']);
-currenciesUnique.forEach(function (value, _, map) {
-  console.log(`${value}: ${value}`);
-});
+  
+  // MAP
+  const currencies = new Map([
+    ['USD', 'United States dollar'],
+    ['EUR', 'Euro'],
+    ['GBP', 'Pound sterling'],
+  ]);
+  
+  currencies.forEach(function (value, key, map) {
+    // similiar as in array ( element, index, whole array)
+    console.log(`${key}: ${value}`);
+  });
+  
+  // SET
+  const currenciesUnique = new Set(['USD', 'GBP', 'USD', 'EUR', 'EUR', 'EUR']);
+  currenciesUnique.forEach(function (value, _, map) {
+    console.log(`${value}: ${value}`);
+  });
+  
+  */
