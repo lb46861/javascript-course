fc5f567f089575c48b79ca364ce9fe76a2a5b8a7
unknown <begovic.lovre@gmail.com>
2022-01-07 19:29:10 +0100

    Internationalizing Numbers in Banksit App

--- 05-Numbers_Dates_Intl_Timers/script.js
+++ 05-Numbers_Dates_Intl_Timers/script.js
@@ -85,7 +85,7 @@ const formatMovementDate = function (myDate, locale) {
     Math.round(Math.abs(date2 - date1) / (1000 * 60 * 60 * 24));
 
   const daysPassed = calcDaysPassed(new Date(), myDate);
-  console.log(daysPassed);
+  //console.log(daysPassed);
 
   if (daysPassed === 0) return 'Today';
   if (daysPassed === 1) return 'Yesterday';
@@ -98,6 +98,13 @@ const formatMovementDate = function (myDate, locale) {
   //   return `${day}/${month}/${year}`;
   // }
   return new Intl.DateTimeFormat(locale).format(myDate);
+};
+
+const formatCur = function (value, locale, currency) {
+  return new Intl.NumberFormat(locale, {
+    style: 'currency',
+    currency: `${currency}`,
+  }).format(value);
 };
 
 const displayMovements = function (acc, sort = false) {
@@ -113,13 +120,15 @@ const displayMovements = function (acc, sort = false) {
     const myDate = new Date(acc.movementsDates[i]);
     const displayMyDate = formatMovementDate(myDate, acc.locale);
 
+    const formattedMov = formatCur(mov, acc.locale, acc.currency);
+
     const html = `
       <div class="movements__row">
         <div class="movements__type movements__type--${type}">${
       i + 1
     } ${type}</div>
         <div class=movements__date">${displayMyDate}</div>
-        <div class="movements__value">${mov.toFixed(2)}€</div>
+        <div class="movements__value">${formattedMov}</div>
       </div>
     `;
 
@@ -129,19 +138,19 @@ const displayMovements = function (acc, sort = false) {
 
 const calcDisplayBalance = function (acc) {
   acc.balance = acc.movements.reduce((acc, mov) => acc + mov, 0);
-  labelBalance.textContent = `${acc.balance.toFixed(2)}€`;
+  labelBalance.textContent = formatCur(acc.balance, acc.locale, acc.currency);
 };
 
 const calcDisplaySummary = function (acc) {
   const incomes = acc.movements
     .filter(mov => mov > 0)
     .reduce((acc, mov) => acc + mov, 0);
-  labelSumIn.textContent = `${incomes.toFixed(2)}€`;
+  labelSumIn.textContent = formatCur(incomes, acc.locale, acc.currency);
 
   const out = acc.movements
     .filter(mov => mov < 0)
     .reduce((acc, mov) => acc + mov, 0);
-  labelSumOut.textContent = `${Math.abs(out).toFixed(2)}€`;
+  labelSumOut.textContent = formatCur(Math.abs(out), acc.locale, acc.currency);
 
   const interest = acc.movements
     .filter(mov => mov > 0)
@@ -151,7 +160,7 @@ const calcDisplaySummary = function (acc) {
       return int >= 1;
     })
     .reduce((acc, int) => acc + int, 0);
-  labelSumInterest.textContent = `${interest.toFixed(2)}€`;
+  labelSumInterest.textContent = formatCur(interest, acc.locale, acc.currency);
 };
 
 const createUsernames = function (accs) {
@@ -488,4 +497,22 @@ const days1 = calcDaysPassed(new Date(2037, 3, 4), new Date(2037, 3, 14));
 
 console.log(days1);
 
+
+const num = 3891243.23;
+
+const options = {
+  style: 'currency',
+  unit: 'mile-per-hour',
+  currency: 'EUR',
+  // useGrouping: false,
+};
+
+console.log('US: ', new Intl.NumberFormat('en-US', options).format(num));
+console.log('DE: ', new Intl.NumberFormat('de-DE', options).format(num));
+console.log('SY: ', new Intl.NumberFormat('ar-SY', options).format(num));
+console.log(
+  navigator.language.toUpperCase() + ': ',
+  new Intl.NumberFormat(navigator.language, options).format(num)
+  );
+  
 */
