04cffe98e731aceeade698dcf0341cb3a7d23737
unknown <begovic.lovre@gmail.com>
2022-01-15 15:46:45 +0100

    Constructor function and operator new

--- 07-OOP-With-js/script.js
+++ 07-OOP-With-js/script.js
@@ -1,1 +1,30 @@
 'use strict';
+
+const Person = function (firstName, birthYear) {
+  // Instance properties
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+
+  // Never do this, instead we will use prototypes and prototype inheritance, with this if we have 1000 objects function will be called 1000 times
+  //   this.calcAge = function () {
+  //     console.log(2022 - this.birthYear);
+  //   };
+};
+
+// We call Constructor with NEW keyword
+const jonas = new Person('Jonas', 1991);
+console.log(jonas);
+
+// 1. New {} empty object is created
+// 2. function is called, this keyword is set to new empty object {}
+// 3. {} is linked to a prototype
+// 4. function automatically returns object
+
+const matilda = new Person('Matilda', 2017);
+const jack = new Person('Jack', 1999);
+console.log(matilda, jack);
+
+const jay = 'Jay';
+
+console.log(jonas instanceof Person);
+console.log(jay instanceof Person);
