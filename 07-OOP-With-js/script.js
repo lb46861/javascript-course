b954b0057fa8037def6022fe6ae4f664cadbb259
unknown <begovic.lovre@gmail.com>
2022-01-20 18:50:24 +0100

    Object create

--- 07-OOP-With-js/script.js
+++ 07-OOP-With-js/script.js
@@ -196,3 +196,27 @@ console.log(account.latest);
 
 account.latest = 50;
 console.log(account.movements);
+
+const PersonProto = {
+  calcAge() {
+    console.log(2022 - this.birthYear);
+  },
+
+  init(firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+  },
+};
+
+const steven = Object.create(PersonProto);
+steven.name = 'Steven';
+steven.birthYear = 1999;
+console.log(steven);
+steven.calcAge();
+
+console.log(steven.__proto__);
+console.log(steven.__proto__ === PersonProto);
+
+const sarah = Object.create(PersonProto);
+sarah.init('Sarah', 2000);
+sarah.calcAge();
