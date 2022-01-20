cea388c9d32f428fd818f55e53b661faacfc0fca
unknown <begovic.lovre@gmail.com>
2022-01-20 19:54:56 +0100

    Inheritance between classes - Constructor functions

--- 07-OOP-With-js/script.js
+++ 07-OOP-With-js/script.js
@@ -1,7 +1,7 @@
 'use strict';
 
 /*
- */
+
 const Person = function (firstName, birthYear) {
   // Instance properties
   this.firstName = firstName;
@@ -255,3 +255,41 @@ bmwCl.brake();
 bmwCl.speedUS = 50;
 console.log(bmwCl);
 console.log(bmwCl.speedUS);
+
+ */
+
+const Person = function (firstName, birthYear) {
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+};
+
+Person.prototype.calcAge = function () {
+  console.log(2022 - this.birthYear);
+};
+
+const Student = function (firstName, birthYear, course) {
+  Person.call(this, firstName, birthYear);
+  this.course = course;
+};
+
+// Linking prototypes
+Student.prototype = Object.create(Person.prototype);
+
+Student.prototype.introduce = function () {
+  console.log(`My name is ${this.firstName} and I study ${this.course}`);
+};
+
+const mike = new Student('Mike', 2000, 'Computer Science');
+console.log(mike);
+mike.introduce();
+mike.calcAge();
+
+console.log(mike.__proto__);
+console.log(mike.__proto__.__proto__);
+
+console.log(mike instanceof Student);
+console.log(mike instanceof Person);
+console.log(mike instanceof Object);
+
+Student.prototype.constructor = Student;
+console.dir(Student.prototype.constructor);
