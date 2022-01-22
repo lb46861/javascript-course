10b2d73448f71c5bdad1e3f414487ba4ed997641
unknown <begovic.lovre@gmail.com>
2022-01-23 00:17:08 +0100

    Encapsulation - Private Class Fields and Methods

--- 07-OOP-With-js/script.js
+++ 07-OOP-With-js/script.js
@@ -441,34 +441,44 @@ jay.calcAge();
 
  */
 
+// 1) Public fields
+// 2) Private fields
+// 3) Public methods
+// 4) Private methods
+// (there is also the static version)
+
 class Account {
+  // 1) Public fields (instances)
+  locale = navigator.language;
+
+  // 2) Private fields (instances)
+  #movements = [];
+  #pin;
+
   constructor(owner, currency, pin) {
     this.owner = owner;
     this.currency = currency;
-    this._pin = pin;
+    this.#pin = pin;
     // protected property
-    this._movements = [];
-    this.locale = navigator.language;
+    // this._movements = [];
+    // this.locale = navigator.language;
 
     console.log(`Thanks for opening an account, ${owner}`);
   }
 
+  // 3) Public methods
   // Public interface
 
   getMovements() {
-    return this._movements;
+    return this.#movements;
   }
 
   deposit(val) {
-    this._movements.push(val);
+    this.#movements.push(val);
   }
 
   withdraw(val) {
     this.deposit(-val);
-  }
-
-  _approveLoan(val) {
-    return true;
   }
 
   requestLoan(val) {
@@ -476,6 +486,16 @@ class Account {
       this.deposit(val);
       console.log(`Loan approved !`);
     }
+  }
+
+  static helper() {
+    console.log('Hello, I am here to help!');
+  }
+
+  // 4) Private methods - not yet implemented
+  // #approveLoan(val) {
+  _approveLoan(val) {
+    return true;
   }
 }
 
@@ -487,7 +507,10 @@ console.log(acc1);
 acc1.deposit(250);
 acc1.withdraw(140);
 acc1.requestLoan(1000);
-console.log(acc1._approveLoan(1000));
 console.log(acc1.getMovements());
 console.log(acc1);
-console.log(acc1.pin);
+
+// console.log(acc1.#movements);
+// console.log(acc1.#pin);
+// console.log(acc1.#approveLoan(100));
+Account.helper();
