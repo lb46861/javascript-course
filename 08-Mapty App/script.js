b52f34cff2e63f0bca567ec787f3e0ba018a80dd
unknown <begovic.lovre@gmail.com>
2022-01-23 18:04:51 +0100

    Displaying Map Marker

--- 08-Mapty App/script.js
+++ 08-Mapty App/script.js
@@ -23,16 +23,30 @@ if (navigator.geolocation) {
 
       // L is namespace for Leaflet geolocation library
       const map = L.map('map').setView(coords, 14);
+      //console.log(map);
 
       L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
         attribution:
           '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
       }).addTo(map);
 
-      L.marker(coords)
-        .addTo(map)
-        .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-        .openPopup();
+      map.on('click', function (mapEvent) {
+        //console.log(mapEvent);
+        const { lat, lng } = mapEvent.latlng;
+        L.marker([lat, lng])
+          .addTo(map)
+          .bindPopup(
+            L.popup({
+              maxWidth: 250,
+              minWidth: 100,
+              autoClose: false,
+              closeOnClick: false,
+              className: 'running-popup',
+            })
+          )
+          .setPopupContent('Workout')
+          .openPopup();
+      });
     },
     function () {
       alert('Could not get your positon!');
