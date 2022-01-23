d601e928be220b9a447ac44b398aea0f76c3e649
unknown <begovic.lovre@gmail.com>
2022-01-23 13:22:19 +0100

    Coding challenge 4, more ES6 Class examples

--- 07-OOP-With-js/script.js
+++ 07-OOP-With-js/script.js
@@ -439,7 +439,7 @@ jay.init('Jay', 2000, 'Computed Science');
 jay.introduce();
 jay.calcAge();
 
- */
+
 
 // 1) Public fields
 // 2) Private fields
@@ -521,3 +521,78 @@ Account.helper();
 // Chaining
 acc1.deposit(300).deposit(500).withdraw(35).requestLoan(25000).withdraw(4000);
 console.log(acc1.getMovements());
+
+ */
+
+// Coding Challenge #4
+// 1. Re-create Challenge #3, but this time using ES6 classes: create an 'EVCl' child class of the 'CarCl' class
+// 2. Make the 'charge' property private
+// 3. Implement the ability to chain the 'accelerate' and 'chargeBattery'
+// methods of this class, and also update the 'brake' method in the 'CarCl' class. Then experiment with chaining!
+
+// Test data:
+// § Data car 1: 'Rivian' going at 120 km/h, with a charge of 23
+
+class CarCl {
+  constructor(make, speed) {
+    this.make = make;
+    this.speed = speed;
+  }
+
+  accelerate() {
+    this.speed += 10;
+    console.log(`Rivian is currently driving ${this.speed}km/h.`);
+  }
+
+  brake() {
+    this.speed -= 5;
+    console.log(`Rivian is currently driving ${this.speed}km/h.`);
+    return this;
+  }
+
+  get speedUS() {
+    return this.speed / 1.6;
+  }
+
+  set speedUS(speed) {
+    this.speed = speed * 1.6;
+  }
+}
+
+class EVCl extends CarCl {
+  #charge;
+
+  constructor(make, speed, charge) {
+    super(make, speed);
+    this.#charge = charge;
+  }
+
+  chargeBattery(chargeTo) {
+    this.#charge = chargeTo;
+    return this;
+  }
+
+  accelerate() {
+    this.speed += 20;
+    this.#charge--;
+    console.log(
+      `Rivian is currently driving ${this.speed}km/h with a charge of ${
+        this.#charge
+      }`
+    );
+    return this;
+  }
+}
+
+const car = new CarCl(10, 50);
+console.log(car);
+const rivian = new EVCl('Rivian', 120, 23);
+rivian
+  .accelerate()
+  .accelerate()
+  .accelerate()
+  .brake()
+  .chargeBattery(50)
+  .accelerate();
+
+console.log(rivian.speedUS);
