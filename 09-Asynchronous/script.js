3dc1e7dcb758ea44c9440825e0e40c3949881b13
unknown <begovic.lovre@gmail.com>
2022-02-04 00:57:35 +0100

    Consuming promises

--- 09-Asynchronous/script.js
+++ 09-Asynchronous/script.js
@@ -66,6 +66,8 @@ const renderCountry = function (data, className = '') {
   countriesContainer.style.opacity = 1;
 };
 
+/*
+
 const getCountryAndNeighbour = function (country) {
   // AJAX call country 1
   const request = new XMLHttpRequest();
@@ -112,3 +114,38 @@ setTimeout(() => {
     }, 1000);
   }, 1000);
 }, 1000);
+
+
+
+// OLD WAY
+// const request = new XMLHttpRequest();
+// request.open('GET', `https://restcountries.com/v2/name/${country}`);
+// request.send();
+
+// BETTER WAY
+const request = fetch('https://restcountries.com/v2/name/portugal');
+console.log(request);
+
+*/
+
+// const getCountryData = function (country) {
+//   fetch(`https://restcountries.com/v2/name/${country}`)
+//     .then(function (response) {
+//       console.log(response);
+//       return response.json();
+//     })
+//     .then(function (data) {
+//       console.log(data);
+//       const [countryData] = data;
+//       renderCountry(countryData);
+//     });
+// };
+
+// Symplified solution
+const getCountryData = function (country) {
+  fetch(`https://restcountries.com/v2/name/${country}`)
+    .then(response => response.json())
+    .then(data => renderCountry(data[0]));
+};
+
+getCountryData('portugal');
