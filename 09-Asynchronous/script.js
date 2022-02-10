c12875f50d918e5f67b4a84967c8b2ca58f91648
unknown <begovic.lovre@gmail.com>
2022-02-10 02:07:24 +0100

    Reverse Geocoding

--- 09-Asynchronous/script.js
+++ 09-Asynchronous/script.js
@@ -132,7 +132,7 @@ setTimeout(() => {
 const request = fetch('https://restcountries.com/v2/name/portugal');
 console.log(request);
 
-*/
+
 
 // const getCountryData = function (country) {
 //   fetch(`https://restcountries.com/v2/name/${country}`)
@@ -190,3 +190,32 @@ btn.addEventListener('click', function () {
 });
 
 getCountryData('australia');
+
+*/
+
+// CODING CHALLENGE #1
+const whereAmI = function (lat, lng) {
+  fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`)
+    .then(res => {
+      if (!res.ok) throw new Error(`Problem with geocoding ${res.status}`);
+      return res.json();
+    })
+    .then(data => {
+      console.log(data);
+      console.log(`You are in ${data.city}, ${data.country}`);
+
+      return fetch(`https://restcountries.com/v3.1/name/${data.country}`);
+    })
+    .then(res => {
+      if (!res.ok) throw new Error(`Country not found ${res.status}`);
+
+      return res.json();
+    })
+    .then(data => renderCountry(data[0]))
+    .catch(err => console.error(`${err.message} 💣`))
+    .finally((countriesContainer.style.opacity = 1));
+};
+
+whereAmI(52.508, 13.381);
+whereAmI(19.037, 72.873);
+whereAmI(-33.933, 18.474);
