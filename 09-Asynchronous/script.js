4cd52c8faec526bf80809b96564969921375dbd8
unknown <begovic.lovre@gmail.com>
2022-02-20 16:36:13 +0100

    Building Simple Promises

--- 09-Asynchronous/script.js
+++ 09-Asynchronous/script.js
@@ -220,7 +220,7 @@ const whereAmI = function (lat, lng) {
 whereAmI(52.508, 13.381);
 whereAmI(19.037, 72.873);
 whereAmI(-33.933, 18.474);
-*/
+
 
 console.log('Test start');
 setTimeout(() => console.log('0 sec timer'), 0);
@@ -231,3 +231,36 @@ Promise.resolve('Resolved promsie 2').then(res => {
   console.log(res);
 });
 console.log('Test end');
+
+*/
+
+const lotteryPromise = new Promise(function (resolve, reject) {
+  console.log('Lottery draw is happening...');
+  setTimeout(function () {
+    if (Math.random() >= 0.5) {
+      resolve('You won 💰');
+    } else {
+      reject(new Error('You lost your money 😭'));
+    }
+  }, 2000);
+});
+
+lotteryPromise.then(res => console.log(res)).catch(err => console.error(err));
+
+// Promisifying setTimeout
+const wait = function (seconds) {
+  return new Promise(function (resolve) {
+    setTimeout(resolve, seconds * 1000);
+  });
+};
+
+wait(2)
+  .then(() => {
+    console.log('I waited for 2 seconds');
+
+    return wait(1);
+  })
+  .then(() => console.log('I waited for 1 second'));
+
+Promise.resolve('Nice').then(x => console.log(x));
+Promise.reject(new Error('Problem')).catch(x => console.error(x));
