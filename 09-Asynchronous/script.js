162b97000e90186bc5af65b608494e4b07b9555a
unknown <begovic.lovre@gmail.com>
2022-02-21 17:35:53 +0100

    Promisifying geolocation

--- 09-Asynchronous/script.js
+++ 09-Asynchronous/script.js
@@ -232,7 +232,7 @@ Promise.resolve('Resolved promsie 2').then(res => {
 });
 console.log('Test end');
 
-*/
+
 
 const lotteryPromise = new Promise(function (resolve, reject) {
   console.log('Lottery draw is happening...');
@@ -264,3 +264,47 @@ wait(2)
 
 Promise.resolve('Nice').then(x => console.log(x));
 Promise.reject(new Error('Problem')).catch(x => console.error(x));
+
+*/
+
+const getPosition = function () {
+  return new Promise(function (resolve, reject) {
+    // navigator.geolocation.getCurrentPosition(
+    //   position => resolve(position),
+    //   err => reject(err)
+    // );
+    navigator.geolocation.getCurrentPosition(resolve, reject);
+  });
+};
+
+// getPosition().then(pos => console.log(pos));
+
+const whereAmI = function () {
+  getPosition()
+    .then(pos => {
+      const { latitude: lat, longitude: lng } = pos.coords;
+
+      return fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`);
+    })
+    .then(res => {
+      if (!res.ok) throw new Error(`Problem with geocoding ${res.status}`);
+      console.log(res);
+      return res.json();
+    })
+    .then(data => {
+      console.log(data);
+      console.log(`You are in ${data.city}, ${data.country}`);
+
+      return fetch(`https://restcountries.com/v3.1/name/${data.country}`);
+    })
+    .then(res => {
+      if (!res.ok) throw new Error(`Country not found ${res.status}`);
+
+      return res.json();
+    })
+    .then(data => renderCountry(data[0]))
+    .catch(err => console.error(`${err.message} 💣`))
+    .finally((countriesContainer.style.opacity = 1));
+};
+
+btn.addEventListener('click', whereAmI);
