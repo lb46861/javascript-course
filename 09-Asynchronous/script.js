955b23dc159e099a95839df4131f93e644b6b2f6
unknown <begovic.lovre@gmail.com>
2022-02-27 15:01:44 +0100

    Parallel Promises

--- 09-Asynchronous/script.js
+++ 09-Asynchronous/script.js
@@ -356,7 +356,7 @@ createImage('img/img-1.jpg')
   })
   .catch(err => console.error(err));
 
-*/
+
 
 const getPosition = function () {
   return new Promise(function (resolve, reject) {
@@ -372,11 +372,6 @@ const whereAmI = async function () {
     const resGeo = await fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`);
     if (!resGeo.ok) throw new Error(`Problem getting location data`);
     const dataGeo = await resGeo.json();
-    console.log(dataGeo);
-
-    // fetch(`https://restcountries.com/v2/name/${country}`).then(res =>
-    //   console.log(res)
-    // );
 
     //Simpler way
     const res = await fetch(
@@ -384,18 +379,38 @@ const whereAmI = async function () {
     );
     if (!res.ok) throw new Error(`Problem getting country`);
     const data = await res.json();
-    console.log(data);
     renderCountry(data[0]);
+
+    return `You are in ${dataGeo.city}, ${dataGeo.country}`;
   } catch (err) {
     console.error(`${err} 💣`);
     renderError(`Something went wrong 💣 ${err.message}`);
+
+    // Reject promise returned from async function
+    throw err;
   }
 
   countriesContainer.style.opacity = 1;
 };
-whereAmI();
 
-console.log(1);
+console.log('1: Will get location');
+// const city = whereAmI();
+// console.log(city);
+
+// whereAmI()
+//   .then(city => console.log(`2: ${city} `))
+//   .catch(err => console.error(`2: ${err.message} 💣`))
+//   .finally(() => console.log('3: Finished getting location'));
+
+(async function () {
+  try {
+    const city = await whereAmI();
+    console.log(`2: ${city}`);
+  } catch (err) {
+    console.error(`2: ${err.message} 💣`);
+  }
+  console.log('3: Finished getting location');
+})();
 
 // try {
 //   let y = 1;
@@ -404,3 +419,34 @@ console.log(1);
 // } catch (err) {
 //   alert(err.message);
 // }
+
+
+*/
+
+const getJSON = function (url, errorMsg = 'Something went wrong') {
+  return fetch(url).then(response => {
+    if (!response.ok) throw new Error(`${errorMsg} (${response.status})`);
+
+    return response.json();
+  });
+};
+
+const get3Countries = async function (c1, c2, c3) {
+  try {
+    // const [data1] = await getJSON(`https://restcountries.com/v3.1/name/${c1}`);
+    // const [data2] = await getJSON(`https://restcountries.com/v3.1/name/${c2}`);
+    // const [data3] = await getJSON(`https://restcountries.com/v3.1/name/${c3}`);
+
+    const data = await Promise.all([
+      getJSON(`https://restcountries.com/v3.1/name/${c1}`),
+      getJSON(`https://restcountries.com/v3.1/name/${c2}`),
+      getJSON(`https://restcountries.com/v3.1/name/${c3}`),
+    ]);
+    console.log(data.map(d => d[0].capital));
+    // console.log([data1.capital, data2.capital, data3.capital]);
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+get3Countries('portugal', 'canada', 'ukraine');
